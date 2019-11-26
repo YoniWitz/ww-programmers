8b5f56a0fbf08a4dab3622600410f72c99219495
Yoni Hirshowitz <jonhirsh39@gmail.com>
2019-11-26 18:05:05 -0500

    added sulgify and email confirmation

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -4,15 +4,15 @@
       <h2 class="center deep-purple-text">Signup</h2>
       <div class="field">
         <label for="email">Email:</label>
-        <input type="email" name="email" v:model="email" />
+        <input type="email" name="email" v-model="email" />
       </div>
       <div class="field">
         <label for="password">Password:</label>
-        <input type="password" name="password" v:model="password" />
+        <input type="password" name="password" v-model="password" />
       </div>
       <div class="field">
         <label for="alias">Alias:</label>
-        <input type="text" name="alias" v:model="alias" />
+        <input type="text" name="alias"  v-bind:value="alias" disabled/>
       </div>
       <p class="red-text center">{{feedback}}</p>
      
@@ -24,6 +24,8 @@
 </template>
 
 <script>
+import slugify from 'slugify'
+import db from '@/Firebase/init'
 export default {
   name: "Signup",
   data() {
@@ -31,18 +33,40 @@ export default {
       email: "",
       password: "",
       alias: "",
-      feedback: ""
+      feedback: "",
+      slug:""
     };
   },
   methods: {
     signup() {
-      if (this.alias) {
+      if (this.alias && this.feedback ==="") {
+         this.slug=alias;          
       } else {
-        this.feedback = "Please enter an alias";
+        this.feedback = "Please enter unused email";
       }
+    }
+  },
+  watch:{
+    email(newEmail, oldEmail){
+      this.alias= slugify(newEmail, {
+        replacement: '-',
+        remove: /[$*_+~.()'"!\-:@']/g,
+        lower:true
+      })
+
+      db.collection('users').doc(this.alias).get()
+      .then(doc => {
+        if(doc.exists){
+          this.feedback = "this email is already used";
+        }
+        else{
+          this.feedback = "go ahead, sign up";
+        }
+      })
     }
   }
 };
+
 </script>
 
 <style scoped>
