df613648506df6120df25a5c4ae1ac84ef5c79b4
Yoni Hirshowitz <jonhirsh39@gmail.com>
2019-11-27 11:48:59 -0500

    add wrapper and user info

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import firebase from 'firebase'
 export default {
   name: "GMap",
   data() {
@@ -15,6 +16,7 @@ export default {
   },
   mounted() {
     this.renderMap();
+    console.log(firebase.auth().currentUser)
   },
   methods: {
     renderMap() {
