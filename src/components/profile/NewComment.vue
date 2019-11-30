bc3081a5171f7fbbf5d5d07298221bb0ea5eb49e
Yoni Hirshowitz <jonhirsh39@gmail.com>
2019-11-30 18:09:19 -0500

    finished profile page

--- src/components/profile/NewComment.vue
+++ src/components/profile/NewComment.vue
@@ -1,45 +1,47 @@
 <template>
-    <div class="new-comment">
-        <form @submit.prevent="addComment">
-            <label for="new-comment">New Comment (press enter to add comment)</label>
-            <input type=text name="new-comment" v-model="newComment"/> 
-            <p class="red-text">{{feedback}}</p>
-        </form>
-    </div>
+  <form @submit.prevent="addComment">
+    <label for="new-comment">New Comment</label>
+    <input type="text" name="new-comment" v-model="newComment" />
+    <p class="red-text">{{feedback}}</p>
+    <button type="submit">Send Comment</button>
+  </form>
 </template>
 
 <script>
-import db from '@/Firebase/init.js'
+import db from "@/firebase/init.js";
 export default {
-    name:'NewComment',
-    props:['alias'],
-    data(){
-        return {
-            newComment:"",
-            feedback: ""
-        }
-    },
-    methods:{
-        addMessage(){
-            this.feedback = "";
-            if(this.newMessage){
-                db.collection('comments').add({                  
-                    comment : this.newMessage,
-                    time : Date.now(),
-                    from : this.alias,
-                    to: this.alias
-                }).then(() =>{
-                    this.newComment = ""
-                }).catch(err =>{
-                    console.log('error');
-                })
-            }
-            else{
-                this.feedback = "You must enter a comment";
-            }
-        }
+  name: "NewComment",
+  props: ["user", "profile"],
+  data() {
+    return {
+      newComment: "",
+      feedback: ""
+    };
+  },
+  methods: {
+    addComment() {
+      this.feedback = "";
+      if (this.newComment) {
+        //save new comment
+        db.collection("comments")
+          .add({
+            to: this.profile.id,
+            from: this.user.id,
+            comment: this.newComment,
+            time: Date.now()
+          })
+          .then(() => {
+            this.newComment = "";
+          })
+          .catch(err => {
+            console.log("error");
+          });
+      } else {
+        this.feedback = "You must enter a message";
+      }
     }
-}
+  }
+};
 </script>
 
 <style scoped>
--- src/components/profile/ViewProfile.vue
+++ src/components/profile/ViewProfile.vue
@@ -1,86 +1,87 @@
 <template>
-    <div class="view-profile container">
-        <div v-if="profile" class="card">
-            <h2 class="deep-purple-text center">{{profile.alias}}'s profile</h2>
-            <ul v-for="(comment,index) in comments" v-bind:key=index>
-                <li>{{comment.from}} : {{comment.comment}}</li>
-            </ul>
-        </div>
-          <form @submit.prevent="addComment">         
-            <label for="new-comment">New Comment</label>
-            <input type=text name="new-comment" v-model="newComment"/> 
-             <p class="red-text">{{feedback}}</p>
-            <button type="submit">Send Comment</button>
-        </form>
+  <div class="view-profile container">
+    <div v-if="profile" class="card">
+      <h2 class="deep-purple-text center">{{profile.alias}}'s profile</h2>
+      <ul class="comments collection" >
+        <li v-for="(comment, index) in comments" v-bind:key="index">
+            <div class="deep-purple-text"> 
+                {{comment.from}}: <span class="grey-text text-darken-2">{{comment.comment}}</span>
+            </div>
+        </li>
+      </ul>
     </div>
+    <NewComment :profile="profile" :user="user" />
+  </div>
 </template>
 
 <script>
-import firebase from 'firebase'
-import db from '@/firebase/init.js'
-import NewMessage from '../profile/NewComment.vue'
+import firebase from "firebase";
+import db from "@/firebase/init.js";
+import NewComment from "@/components/profile/NewComment.vue";
 export default {
-    name: 'ViewProfile',
-    data(){
-        return{
-            profile: null,
-            comments:[],
-            feedback:"",
-            user:null,
-            newComment: ""
-        }
+  name: "ViewProfile",
+  components:{
+      NewComment
     },
-    created(){
-        let profile_id = this.$route.params.id;
+  data() {
+    return {
+      profile: null,
+      comments: [],
+      
+      user: null,
+     
+    };
+  },
+  created() {
+    let profile_id = this.$route.params.id;
 
-        //get profile
-        db.collection('users').doc(profile_id)
-        .get()
-        .then(profile => this.profile = profile.data());
+    //get profile
+    db.collection("users")
+      .doc(profile_id)
+      .get()
+      .then(profile => {
+          this.profile = profile.data();
+          this.profile.id = profile.id;
+      });
 
-        //get comments
-        db.collection('comments').where('to', '==', profile_id)
-        .get()
-        .then(comments =>{
-            comments.forEach(comment => {
-                let temp = comment.data();
-                temp.id = comment.id;
-                this.comments.push(temp);
-            })
+    //get comments
+    // db.collection("comments")
+    //   .where("to", "==", profile_id)
+    //   .get()
+    //   .then(comments => {
+    //     comments.forEach(comment => {
+    //       let temp = comment.data();
+    //       temp.id = comment.id;
+    //       this.comments.push(temp);
+    //     });
+    //   });
+
+    db.collection('comments')
+    .where("to", "==", profile_id)
+    //.orderBy('time')
+    .onSnapshot(snapshot => {
+        snapshot.docChanges()
+        .forEach(change =>{
+            if(change.type === 'added'){
+                let doc = change.doc.data();
+                doc.id = change.doc.id;
+                this.comments.push(doc);
+            }
         })
+    })
 
-        //get user
-        db.collection('users').where('user_id', '==', firebase.auth().currentUser.uid)
-        .get().then(users => {
-            this.user = users.docs[0].data();
-            this.user.id = users.docs[0].id;
-        }).catch(err => console.log(err));
-        
-    },
-    methods:{
-        addComment(){
-            this.feedback = "";
-            if(this.newComment){
-                //save new comment
-                db.collection('comments').add({
-                    to : this.profile.id,
-                    from: this.user.id,
-                    comment : this.newComment,
-                    time : Date.now()
-                }).then(() =>{
-                    this.newComment = ""
-                }).catch(err =>{
-                    console.log('error');
-                })
-            }
-            else{
-                this.feedback = "You must enter a message";
-            }
-        }
-    }
-}
+    //get user
+    db.collection("users")
+      .where("user_id", "==", firebase.auth().currentUser.uid)
+      .get()
+      .then(users => {
+        this.user = users.docs[0].data();
+        this.user.id = users.docs[0].id;
+      })
+      .catch(err => console.log(err));
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
